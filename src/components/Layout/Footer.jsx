56e7293f0e07a5a1f8d124c7051d92ab18e15497
heaminjo <157016835+heaminjo@users.noreply.github.com>
2025-05-16 09:53:37 +0900

    Delete src directory

--- src/components/Layout/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import FooterComp from "./FooterStyle";
-
-export default function Footer() {
-  return (
-    <FooterComp>
-      <p>Footer</p>
-    </FooterComp>
-  );
-}
--- src/page/main/MainStyle.js
+++
@@ -1,9 +0,0 @@
-import styled from "styled-components";
-
-const MainComp = styled.div`
-  .mainInner {
-    margin-top: 130px;
-    height: 700px;
-  }
-`;
-export default MainComp;
