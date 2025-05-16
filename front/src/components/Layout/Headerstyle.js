2de6c973331b71d6bfc8b70113972f2d907f0065
heaminjo <157016835+heaminjo@users.noreply.github.com>
2025-05-16 09:56:23 +0900

    프론트 초기 (레이아웃)

---
+++ front/src/components/Layout/Headerstyle.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const HeaderComp = styled.header`
+  width: 100%;
+  height: 130px;
+  position: fixed;
+  top: 0;
+  background-color: #ccc;
+  h1 {
+    margin: 0;
+  }
+`;
+export default HeaderComp;
