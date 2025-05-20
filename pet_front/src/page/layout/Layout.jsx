5817212fc9f809243c59f5bbaf7b77fc0270a0c6
heaminjo <157016835+heaminjo@users.noreply.github.com>
2025-05-20 22:01:51 +0900

    test

--- pet_front/src/page/layout/Layout.jsx
+++ pet_front/src/page/layout/Layout.jsx
@@ -3,6 +3,7 @@ import Header from "../../components/Layout/Header";
 import Footer from "../../components/Layout/Footer";
 
 export default function Layout() {
+  //ddd
   return (
     <>
       <Header />
