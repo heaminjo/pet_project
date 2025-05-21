5eb8cac73b4ba00f1628cdd5db7714fefe15ac1e
heaminjo <157016835+heaminjo@users.noreply.github.com>
2025-05-21 10:53:56 +0900

    a

--- pet_back/src/main/java/com/example/pet_back/config/WebMvcConfig.java
+++ pet_back/src/main/java/com/example/pet_back/config/WebMvcConfig.java
@@ -6,7 +6,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class WebMvcConfig implements WebMvcConfigurer {
 
     private final long MAX_AGE_SECS = 3600;
-
+    //s
     @Override
     public void addCorsMappings(CorsRegistry registry) {
 
