ee9a32dd94708401e4f88c9dc35c2de1d9fc2237
heaminjo <157016835+heaminjo@users.noreply.github.com>
2025-05-21 11:28:55 +0900

    feat

--- pet_front/src/api/MemberApi.js
+++ pet_front/src/api/MemberApi.js
@@ -1,6 +1,6 @@
-const { default: axios } = require("axios");
+import axios from "axios";
 
-const KH_DOMAIN = "http://localhost:8086";
+const KH_DOMAIN = "http://localhost:8080";
 const MemberApi = {
   //로그인
   login: async (email, password) => {
@@ -10,4 +10,18 @@ const MemberApi = {
     };
     return await axios.post(`${KH_DOMAIN}/auth/login`, login);
   },
+  //이메일 중복 체크
+  dupEmail: async (email) => {
+    const result = await axios.get(
+      `${KH_DOMAIN}/member/emailcheck?email=${email}`
+    );
+    return result.data;
+  },
+
+  //회원가입
+  join: async (newUser) => {
+    const result = await axios.post(`${KH_DOMAIN}/auth/join`, newUser);
+    return result.data;
+  },
 };
+export default MemberApi;
