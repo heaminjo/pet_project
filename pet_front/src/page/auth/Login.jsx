0821cc17ea13d02bf5e28b7e860d2281162d8280
heaminjo <157016835+heaminjo@users.noreply.github.com>
2025-05-21 17:38:09 +0900

    loginForm 초기 완성

--- pet_front/src/page/auth/Login.jsx
+++ pet_front/src/page/auth/Login.jsx
@@ -1,32 +1,81 @@
+import { useForm } from "react-hook-form";
 import LoginComp from "./LoginStyle";
-
+import { yupResolver } from "@hookform/resolvers/yup";
+import * as yup from "yup";
+import MemberApi from "../../api/MemberApi";
+import { useNavigate } from "react-router-dom";
 export default function Login() {
+  const navigate = useNavigate();
+
+  //유효성 조건(yup)
+  const schema = yup.object({
+    email: yup
+      .string()
+      .email("올바른 이메일 형식이 아닙니다.")
+      .required("필수 입력입니다."),
+    password: yup
+      .string()
+      .required("필수 입력입니다.")
+      .matches(
+        /^(?=.*[A-Z])(?=.*[a-z])(?=.*\d)(?=.*[!@#$%^&*()_+{}\[\]:;<>,.?/~`\-=|\\]).{10,16}$/,
+        "10~16자 대소문자+영문+특수 기호로 조합해주세요."
+      ),
+  });
+
+  //폼 상태관리
+  const {
+    register, //폼 필드와 리액트 훅 폼을 연결
+    watch, //특정 watch("email") 입력값들을 실시간 감시
+    handleSubmit,
+    trigger, //실시간 검사
+    setValue,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+    mode: "onBlur", // 실시간 검사
+  });
+
+  //로그인 버튼 클릭
+  const clickLogin = async () => {
+    const result = await MemberApi.login(watch("email"), watch("password"));
+    if (result) {
+      alert("로그인 성공!");
+      navigate("/");
+    } else {
+      alert("로그인 실패");
+    }
+  };
   return (
     <LoginComp>
       <div className="login_inner">
         <div className="login_container">
           <h2>로그인</h2>
-          <form>
-            <table>
-              <tbody>
-                <tr>
-                  <th>
-                    <label htmlFor="email">이메일</label>
-                  </th>
-                  <td id="td_email">
-                    <input type="email" id="email" name="email" />
-                  </td>
-                </tr>
-                <tr>
-                  <th>
-                    <label htmlFor="email">비밀번호</label>
-                  </th>
-                  <td>
-                    <input type="password" id="password" name="password" />
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+          <form onSubmit={handleSubmit(() => clickLogin())}>
+            <ul className="login_form">
+              <li>
+                <input
+                  type="email"
+                  placeholder="Email"
+                  {...register("email")}
+                />
+                {errors.email && (
+                  <p className="error_message">{errors.email.message}</p>
+                )}
+              </li>
+              <li>
+                <input
+                  type="password"
+                  placeholder="Password"
+                  {...register("password")}
+                />
+                {errors.password && (
+                  <p className="error_message">{errors.password.message}</p>
+                )}
+              </li>
+            </ul>
+            <button type="submit" onClick={() => clickLogin()}>
+              로그인
+            </button>
           </form>
         </div>
       </div>
--- pet_front/src/page/auth/LoginStyle.js
+++ pet_front/src/page/auth/LoginStyle.js
@@ -10,20 +10,35 @@ const LoginComp = styled.div`
     height: 100%;
     margin: 0 auto;
     .login_container {
+      padding-top: 130px;
+      width: 350px;
+      margin: 0 auto;
       h2 {
         margin: 20px 0;
-        text-align: center;
+        text-align: start;
       }
-      table {
-        margin: 0 auto;
-        tr {
-          height: 50px;
-          input {
-            height: 35px;
-            width: 360px;
-            padding-left: 10px;
-          }
+      .login_form {
+        display: flex;
+        flex-direction: column;
+        gap: 10px;
+        height: 150px;
+        input {
+          height: 40px;
+          width: 100%;
+          padding-left: 10px;
         }
+        p {
+          color: red;
+          font-size: 13px;
+          font-weight: bold;
+        }
+      }
+      button {
+        height: 40px;
+        width: 100%;
+        background-color: #f8e776;
+        border: none;
+        cursor: pointer;
       }
     }
   }
