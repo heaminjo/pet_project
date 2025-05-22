20c922938d07699194e48b4e492e6d8c09df2281
heaminjo <157016835+heaminjo@users.noreply.github.com>
2025-05-22 16:24:31 +0900

    TokenProvider 수정
    accessToken과 refreshToken+access을 만드는 메서드를 각각 생성

--- pet_back/src/main/java/com/example/pet_back/jwt/TokenProvider.java
+++ pet_back/src/main/java/com/example/pet_back/jwt/TokenProvider.java
@@ -5,6 +5,8 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -24,30 +26,42 @@ public class TokenProvider {
     //보안 강도가 높다.
     private final SecretKey SECRET_KEY = Keys.hmacShaKeyFor("petSecuritySecretkey0702_superkey!".getBytes());
     private static final String BEARER_TYPE = "Bearer"; // 토큰이 어떤 방식으로 발급되었는지
+    private static final long ACCESS_TOKEN_EXPIRE_TIME = 1000 * 60 * 30;            // 30분
+    private static final long REFRESH_TOKEN_EXPIRE_TIME = 1000 * 60 * 60 * 24 * 7;  // 7일
 
-    //토큰 생성
-    public TokenDTO createToken(Map<String,Object> claimsList){
+    //AccessToken만 생성
+    public TokenDTO createToken(Authentication authentication){
+
+        //사용자 인증 객체에서 권한을 가져온다.
+        String role = authentication.getAuthorities().stream()
+                .findFirst()
+                .map(GrantedAuthority::getAuthority)
+                .orElse(null); //없을 경우 null
+
         //유효 시간 설정
-        //토큰의 유효시간은 생성된 시점에서 1일로 한다
-        Date expiryDate = Date.from(
-                Instant.now().plus(1, ChronoUnit.DAYS));
+        //Acc토큰의 유효시간은 생성된 시점에서 30qns로 한다
 
 
         //Jwts는 토큰 생성 보관을 지원하는 클래스이다.
         //Json 생성,서명,인코딩,디코딩,파싱등  토큰 관리 기능 제공
+
+        //만료시간 30 분
+        long now = (new Date()).getTime();
+        Date accessTokenExpiresIn = new Date(now + ACCESS_TOKEN_EXPIRE_TIME);
 
         //accessToken(사용자 정보,권한 정보,만료시간)
         String accessToken =  Jwts.builder()
                 //header에 들어갈 내용 및 서명을 위한 비밀키
                 //HS512 알고리즘으로 서명한다.
                 .signWith(SECRET_KEY,SignatureAlgorithm.HS512)
-                .setClaims(claimsList)
+                .claim("role",role)  //payload에 권한 저장
+                .setSubject(authentication.getName()) // payload에 이메일 저장
                 //토큰을 발급한 주체
                 .setIssuer("몽냥마켓")
                 //토큰 발급시간
                 .setIssuedAt(new Date())
                 //토큰 만료 시간
-                .setExpiration(expiryDate)
+                .setExpiration(accessTokenExpiresIn)
                 .compact();
 
         log.info("accessToken 생성 => ",accessToken);
@@ -55,10 +69,51 @@ public class TokenProvider {
         return TokenDTO.builder()
                 .grantType(BEARER_TYPE)
                 .accessToken(accessToken)
-                .accessTokenExpiresln(expiryDate.getTime()) //만료시간
+                .accessTokenExpiresln(accessTokenExpiresIn.getTime()) //만료시간
                 .refreshToken(null)
                 .build();
     }
+
+    //로그인 직후 RefreshToken+Access 토큰 둘다 발급
+    public TokenDTO generateTokenDto(Authentication authentication){
+
+        //사용자 인증 객체에서 권한을 가져온다.
+        String role = authentication.getAuthorities().stream()
+                .findFirst()
+                .map(GrantedAuthority::getAuthority)
+                .orElse(null); //없을 경우 null
+
+
+        long now = (new Date()).getTime();
+
+        //AccessToken 생성
+        //만료시간 30분
+        Date accessTokenExpiresIn = new Date(now + ACCESS_TOKEN_EXPIRE_TIME);
+        String accessToken =  Jwts.builder()
+                .signWith(SECRET_KEY,SignatureAlgorithm.HS512)
+                .claim("role",role)  //payload에 권한 저장
+                .setSubject(authentication.getName()) // payload에 이메일 저장
+                .setIssuer("몽냥마켓")
+                .setIssuedAt(new Date())
+                .setExpiration(accessTokenExpiresIn)
+                .compact();
+
+        //RefreshToken 생성
+        //만료 기간 7일
+        Date refreshTokenExpiresIn = new Date(now + REFRESH_TOKEN_EXPIRE_TIME);
+        String refreshToken = Jwts.builder()
+                .setExpiration(refreshTokenExpiresIn)
+                .signWith(SECRET_KEY,SignatureAlgorithm.HS512)
+                .compact();
+
+        return TokenDTO.builder()
+                .grantType(BEARER_TYPE)
+                .accessToken(accessToken)
+                .accessTokenExpiresln(accessTokenExpiresIn.getTime())
+                .refreshToken(refreshToken)
+                .build();
+    }
+
 
     //전달 받은 토큰 검증
     //Claim 정보 (사용자 정보, 만료 시간 등)을 추출해서 Map<String,Object> 형태로 반환
--- pet_back/src/main/java/com/example/pet_back/service/AuthServiceImpl.java
+++ pet_back/src/main/java/com/example/pet_back/service/AuthServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.pet_back.service;
 import com.example.pet_back.domain.custom.ApiResponse;
 import com.example.pet_back.domain.custom.ErrorResponse;
 import com.example.pet_back.domain.login.LoginRequestDTO;
+import com.example.pet_back.domain.login.TokenDTO;
 import com.example.pet_back.domain.member.MemberRequestDTO;
 import com.example.pet_back.entity.Address;
 import com.example.pet_back.entity.Member;
@@ -15,9 +16,13 @@ import lombok.extern.log4j.Log4j2;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.beans.Transient;
 
 @Service
 @RequiredArgsConstructor
@@ -29,9 +34,12 @@ public class AuthServiceImpl implements  AuthService{
     private final MemberRepository memberRepository;
     private final AddressRepository addressRepository;
     private final MemberMapper mapper;
+    //인증 관련 설정을 구성하기 위한 클래스이며 Authentication을 반환
+    private final AuthenticationManagerBuilder authenticationManagerBuilder;
 
     //로그인
     @Override
+
     public ResponseEntity<?> login(LoginRequestDTO dto) {
         //email과 pw기반으로 AuthenticationToken 생성
         UsernamePasswordAuthenticationToken authenticationToken =
@@ -40,11 +48,21 @@ public class AuthServiceImpl implements  AuthService{
                 );
 
         //실제 검증(비밀번호 체크)
+        //사용자의 ID/비밀번호(authenticationToken)를 받아 인증 절차 실행 후 인증객체(Authentication) 생성
+        //여기에서 DB조회가 이루어지며 권한(role)을 포함한 Authentication객체 반환
+        Authentication authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken);
+        log.info("Authentication => " + authentication);
+
+        //인증 정보 기반으로 토큰 생성
+        //로그인 직후 이므로 refreshToken과  AccessToken 모두 생성해서 발급한다.
+//        TokenDTO tokenDTO =
         return null;
     }
 
     //회원가입
     @Override
+    @Transactional
+    //유저와 주소중 하나만 저장되는 것을 방지
     public ResponseEntity<?> join(MemberRequestDTO dto) {
 
         try {
