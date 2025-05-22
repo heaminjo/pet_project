3a79548a0600665fe7df65ee7fd53e7166450599
heaminjo <157016835+heaminjo@users.noreply.github.com>
2025-05-22 12:49:25 +0900

    SecurityConfig 참조 에러 해결

--- pet_back/src/main/java/com/example/pet_back/config/SecurityConfig.java
+++ pet_back/src/main/java/com/example/pet_back/config/SecurityConfig.java
@@ -46,12 +46,16 @@ public class SecurityConfig {
         //1. jwtAuthenticationFilter(jwt 인증 필터)가 먼저 실행 후 요청 헤더의 JWT 토큰 검사 후 인증 객체 생성 혹은 실패 처리
         //2. 그 다음 UsernamePasswordAuthenticationFilter 가 실행 되어 로그인을 처리
         //3. 만약 JWT 토큰이 유효할 경우 이 후 필터는 이미 인증된 상태로 요청 처리(로그인 유지의 경우)
-        //dd
+
 
         //http의 기본 설정들 비활성화
         // httpBasic: 요청마다 사용자 아이디와 비밀번호를 헤더에 담아 보내느 방식. jwt는 해당 방식을 안쓴다.
         // formLogin : 기본 로그인 폼을 비활성화 jwt는 토큰을 헤더에 담아 보내므로 스프링이 제공하는 폼 로그인 UI가 필요없다.
-        // logout : d
+        // logout : 기본 로그아웃 기능을 비활성화 jwt는 세션이 없고 토큰 자체가 인증 수단으로 토큰을 삭제하는 방식으로 처리
+        // csrf : CSRF 보호 기능 비활성화 jwt는 무상태이며 토큰을 헤더에 넣어보내 CSRF 공격 위험이 적다
+        // cors : Cors 설정은 기본 활성화 클라이언트에서 다른 도메인으로 요창할때 CORS 정책에 맞춰야한다.
+        // sessionManagement: 세션을 아예 만들지 않고 서버에 세션 정보를 저장하지 않겠다는 의미 jwt는 서버가 상태를 저장하지 않는 인증 방식이며
+        //      세션을 사용하지 않고 요청마다 JWT 토큰으로 인증
         return http.httpBasic(httpBasic -> httpBasic.disable()) // HTTP 기본 인증 비활성화
                 .formLogin(formLogin -> formLogin.disable()) // formLogin 비활성화
                 .logout(logout -> logout.disable()) // logout 비활성화
@@ -60,15 +64,14 @@ public class SecurityConfig {
                 .sessionManagement(session -> session
                         .sessionCreationPolicy(SessionCreationPolicy.STATELESS)) // 세션 비활성화 (무상태)
 
-
+//인가 설정 경로 별로 권한에 따른 설정
                 .authorizeHttpRequests(auth -> auth
-                        //.requestMatchers(new AntPathRequestMatcher("/auth/memberlist")).hasRole("ADMIN")
-                        //.requestMatchers("/auth/memberlist").hasAuthority("ROLE_ADMIN")
-                        .requestMatchers("/auth/memberlist").hasRole("ADMIN")
-                        .requestMatchers("/user/boardlist").hasRole("MANAGER")
-                        .requestMatchers("/auth/userdetail","/auth/logout").hasRole("USER")
+                        //관리자와 회원의 경로가 나올때마다 즉시 추가
+
+                        //OPTIONS 메서드로 들어오는 모든 요청을 인증없이 허용
+                        //프론트엔드가 API 요청 전 보내는 CORS 사전 확인 요청을 막지 않기 위해
                         .requestMatchers(HttpMethod.OPTIONS ,"/**").permitAll()
-                        //.anyRequest().authenticated()) // 모든 요청 인증 필요
+                        //설정하지 않은 모든 도메인 요청을 허용 (즉 누구나 접근 가능)
                         .anyRequest().permitAll())
                 .build();
     }
--- pet_back/src/main/java/com/example/pet_back/jwt/JwtAuthenticationFilter.java
+++ pet_back/src/main/java/com/example/pet_back/jwt/JwtAuthenticationFilter.java
@@ -1,4 +1,22 @@
 package com.example.pet_back.jwt;
 
-public class JwtAuthenticationFilter {
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@Log4j2
+@RequiredArgsConstructor
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        
+    }
 }
