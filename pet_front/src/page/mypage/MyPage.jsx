5f2c654a42feb3e67de4a8f7d33586de1aa6f49e
heaminjo <157016835+heaminjo@users.noreply.github.com>
2025-05-23 17:56:03 +0900

    마이페이지 사이드 메뉴 UI완성

--- pet_front/src/page/mypage/MyPage.jsx
+++ pet_front/src/page/mypage/MyPage.jsx
@@ -1,5 +1,6 @@
 import { useEffect } from "react";
 import MemberApi from "../../api/MemberApi";
+import MyPageComp from "./MyPageStyle";
 
 export default function MyPage() {
   useEffect(() => {
@@ -10,5 +11,69 @@ export default function MyPage() {
     const result = MemberApi.detail();
     console.log(result);
   };
-  return <></>;
+  return (
+    <MyPageComp>
+      <div className="mypage_inner">
+        <div className="side_menu">
+          <div className="sub_menu" id="user_menu">
+            <p className="menu_title">내 정보</p>
+            <ul>
+              <li>
+                <span>회원 수정</span>
+              </li>
+              <li>
+                <span>비밀번호 변경</span>
+              </li>
+            </ul>
+          </div>
+          <div className="sub_menu" id="goods_menu">
+            <p className="menu_title">상품</p>
+            <ul>
+              <li>
+                <span>찜</span>
+              </li>
+              <li>
+                <span>장바구니</span>
+              </li>
+              <li>
+                <span>최근 본 목록</span>
+              </li>
+              <li>
+                <span>내 후기</span>
+              </li>
+              <li>
+                <span>배송지 관리</span>
+              </li>
+            </ul>
+          </div>
+          <div className="sub_menu" id="order_menu">
+            <p className="menu_title">주문 / 배송</p>
+            <ul>
+              <li>
+                <span>주문 / 배송 내역</span>
+              </li>
+              <li>
+                <span>취소 내역</span>
+              </li>
+            </ul>
+          </div>
+          <div className="sub_menu" id="border_menu">
+            <p className="menu_title">내 게시물</p>
+            <ul>
+              <li>
+                <span>내 게시물 목록</span>
+              </li>
+              <li>
+                <span>북마크</span>
+              </li>
+              <li>
+                <span></span>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <div className="main_container">메인</div>
+      </div>
+    </MyPageComp>
+  );
 }
