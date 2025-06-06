87ecab93dfaaa44b45e5513d8c53d26162351792
heaminjo <157016835+heaminjo@users.noreply.github.com>
2025-06-06 20:10:57 +0900

    카카오 로그인 커밋 누락 코드 수정

--- pet_front/src/App.js
+++ pet_front/src/App.js
@@ -31,7 +31,6 @@ import BoardInsertForm from "./components/Board/BoardInsertForm";
 import BoardEditForm from "./components/Board/BoardEditForm";
 import Error from "./page/main/Error";
 import UserGrade from "./page/admin/UserGrade";
-import KakaoLogin from "./page/main/KakaoLogin";
 
 //컨텍스트(useContext)
 //로그인 여부부를 전역변수로 뿌리기 위한것
