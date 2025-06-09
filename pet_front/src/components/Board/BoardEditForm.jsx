366d5c00cc9006ed7486ff1581d1aa072878d8fb
heaminjo <157016835+heaminjo@users.noreply.github.com>
2025-06-09 14:59:20 +0900

    main 병합 충돌 수정

--- pet_front/src/components/Board/BoardEditForm.jsx
+++ pet_front/src/components/Board/BoardEditForm.jsx
@@ -38,12 +38,8 @@ export default function BoardEditForm() {
         },
       });
       alert("게시글이 수정되었습니다.");
-<<<<<<< HEAD
-      navigate(`/boardDetail/${category}/${board_id}`);
-=======
-      navigate(`/boardList/${category}`); 
-      
->>>>>>> 남장욱-branch
+
+      navigate(`/boardList/${category}`);
     } catch (err) {
       alert("게시글 수정에 실패했습니다.");
     }
