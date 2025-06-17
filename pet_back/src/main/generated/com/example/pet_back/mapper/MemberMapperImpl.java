310ddafdbab99d2284a0550c9f7820e2e9e7b5a2
CaptianSun <spidermanx77@gmail.com>
2025-06-17 17:55:04 +0900

    게시글 삭제 시 이미지 폴더에서 이미지 삭제

--- pet_back/src/main/generated/com/example/pet_back/mapper/MemberMapperImpl.java
+++ pet_back/src/main/generated/com/example/pet_back/mapper/MemberMapperImpl.java
@@ -12,7 +12,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-06-17T10:57:59+0900",
+    date = "2025-06-17T17:10:26+0900",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.12 (Oracle Corporation)"
 )
 @Component
--- pet_back/src/main/generated/com/example/pet_back/mapper/OrderMapperImpl.java
+++ pet_back/src/main/generated/com/example/pet_back/mapper/OrderMapperImpl.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-06-13T17:07:04+0900",
+    date = "2025-06-17T17:10:26+0900",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 17.0.12 (Oracle Corporation)"
 )
 @Component
