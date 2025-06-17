e85dce3a9e628d9a8627e037fcad99424c8794b2
heaminjo <157016835+heaminjo@users.noreply.github.com>
2025-06-17 12:17:28 +0900

    배너 사진 추가 함수 누락 해결

--- pet_front/src/api/GoodsApi.js
+++ pet_front/src/api/GoodsApi.js
@@ -1,25 +1,27 @@
-import axios from 'axios';
-import instance from '../api/axiosInstance'; // 인스턴스 불러오기
+import axios from "axios";
+import instance from "../api/axiosInstance"; // 인스턴스 불러오기
 
-const KH_DOMAIN = 'http://localhost:8080';
+const KH_DOMAIN = "http://localhost:8080";
 const GoodsApi = {
   // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~ 장 바 구 니 ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   // 장바구니 추가
   addToCart: async (goods) => {
     //alert(`장바구니 담기 => ${goods}`);
 
-    console.log(`장바구니 담기 시도 => ${goods.goodsId}, 수량: ${goods.quantity}`);
-    const result = await instance.post('/cart/add', goods);
+    console.log(
+      `장바구니 담기 시도 => ${goods.goodsId}, 수량: ${goods.quantity}`
+    );
+    const result = await instance.post("/cart/add", goods);
     try {
       if (result.data != null) {
-        console.log('장바구니 담기 완료');
+        console.log("장바구니 담기 완료");
         return result.data;
       } else {
-        console.log('장바구니 담기 실패');
+        console.log("장바구니 담기 실패");
       }
     } catch (err) {
-      console.error('장바구니 추가 실패:', err);
-      alert('장바구니 추가 중 에러가 발생했습니다.');
+      console.error("장바구니 추가 실패:", err);
+      alert("장바구니 추가 중 에러가 발생했습니다.");
     }
   },
 
@@ -49,7 +51,7 @@ const GoodsApi = {
     try {
       const result = await instance.post(`/goods/list`, pages);
       if (result.data != null) {
-        console.log(' getGoodsPageList 응답 결과:', result.data);
+        console.log(" getGoodsPageList 응답 결과:", result.data);
         alert(`getGoodsPageList() 호출됨`);
         return result.data;
       }
@@ -61,31 +63,30 @@ const GoodsApi = {
     try {
       const result = await instance.post(`/goods/favorite`, pages);
       if (result.data != null) {
-        console.log(' getGoodsPageList 응답 결과:', result.data);
+        console.log(" getGoodsPageList 응답 결과:", result.data);
         alert(`getGoodsPageList() 호출됨`);
         return result.data;
       }
     } catch (err) {}
   },
 
-
   // <Goods /> : 상품등록
   regGoods: async (goods) => {
     try {
-      const result = await instance.post('/goods/register', goods);
+      const result = await instance.post("/goods/register", goods);
       if (result.data != null) {
         alert(`상품등록 완료 => ${result.data}`);
         return result.data;
       }
     } catch (err) {
-      console.error('상품 등록 실패:', err);
-      alert('상품 등록 중 에러가 발생했습니다.');
+      console.error("상품 등록 실패:", err);
+      alert("상품 등록 중 에러가 발생했습니다.");
     }
   },
 
   // 상품상세정보 (단일)
   goodsDetail: async () => {
-    const result = await instance.get('/goods/detail/${goods_id}');
+    const result = await instance.get("/goods/detail/${goods_id}");
     return result.data;
   },
 
@@ -95,10 +96,10 @@ const GoodsApi = {
     alert(`getPageList() 호출됨, goodsId = ${JSON.stringify(goodsId)}`);
     const result = await instance.post(`/goods/reviews/${goodsId}`, pages);
     if (result.data != null) {
-      console.log('getReviewsPageList 응답 결과:', result.data);
+      console.log("getReviewsPageList 응답 결과:", result.data);
       return result.data;
     } else {
-      console.log('getReviewsPageList 호출 중 오류 발생:', result.data);
+      console.log("getReviewsPageList 호출 중 오류 발생:", result.data);
       return result.data;
     }
   },
@@ -140,7 +141,13 @@ const GoodsApi = {
   },
   //배너 추가
   bannerInsert: async (newBanner) => {
-    const result = await axios.post(`${KH_DOMAIN}/goods/banner/insert`, newBanner);
+    const formData = new FormData();
+    formData.append("file", newBanner.imageFile);
+    formData.append("position", newBanner.position.toString());
+    const result = await axios.post(
+      `${KH_DOMAIN}/goods/banner/insert`,
+      formData
+    );
     return result.data;
   },
 };
